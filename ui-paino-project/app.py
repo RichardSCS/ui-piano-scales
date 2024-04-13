5ea55b848b9481ffd5adc1cc015e2d7b7cf5d017
PRK2118 <114367128+PRK2118@users.noreply.github.com>
2024-04-13 15:38:16 -0400

    PK nav bar,  flask page and begining of quiz content

---
+++ ui-paino-project/app.py
@@ -0,0 +1,29 @@
+from flask import Flask,redirect,url_for
+from flask import render_template
+from flask import Response, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("home.html")
+
+@app.route('/learn/theory')
+def learn_theory():  # put application's code here
+    return render_template("learn_theory.html")
+
+@app.route('/learn/scales')
+def learn_scales():  # put application's code here
+    return render_template("learn_scales.html")
+
+@app.route('/quiz/theory')
+def quiz_theory():  # put application's code here
+    return render_template("quiz_theory.html")
+
+@app.route('/quiz/scales')
+def quiz_scale():  # put application's code here
+    return render_template("quiz_scales.html")
+
+if __name__ == '__main__':
+    app.run()
