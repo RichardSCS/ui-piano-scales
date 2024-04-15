19dfc06a1e6c7218ae5060d11aa91cd5feb46214
PRK2118 <114367128+PRK2118@users.noreply.github.com>
2024-04-14 21:13:00 -0400

    PK additional scales quiz questions, completed virtual piano HTMl/CSS

--- ui-paino-project/app.py
+++ ui-paino-project/app.py
@@ -21,9 +21,22 @@ def learn_scales():  # put application's code here
 def quiz_theory():  # put application's code here
     return render_template("quiz_theory.html")
 
-@app.route('/quiz/scales')
+@app.route('/quiz/scales/1')
 def quiz_scale():  # put application's code here
     return render_template("quiz_scales.html")
+
+@app.route('/quiz/scales/2')
+def quiz_scale_2():  # put application's code here
+    return render_template("quiz_scales_major.html")
+
+@app.route('/quiz/scales/3')
+def quiz_scale_3():  # put application's code here
+    return render_template("quiz_scales_identify.html")
+
+@app.route('/quiz/scales/4')
+def quiz_scale_4():  # put application's code here
+    return render_template("quiz_scales_2.html")
+
 
 if __name__ == '__main__':
     app.run()
