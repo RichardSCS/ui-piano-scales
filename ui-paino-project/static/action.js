aceb8f13c0f04e078416f16581957570ce1df3a9
Richard Cruz-Silva <rsc2174@columbia.edu>
2024-04-17 13:19:55 -0700

    RCS: added audio files to keyboard to make it playable

--- ui-paino-project/static/action.js
+++ ui-paino-project/static/action.js
@@ -3,7 +3,8 @@ document.querySelectorAll('.key').forEach(key => {
 });
 
 function playNote(note) {
-    const audio = new Audio(`sounds/${note}.wav`); // Ensure you have .wav files named as notes (C.wav, C#.wav, etc.)
+    console.log("playing note " + note)
+    const audio = new Audio(`/static/mp3_Notes/${note}.mp3`); // Ensure you have .wav files named as notes (C.wav, C#.wav, etc.)
     audio.play();
 }
 
@@ -35,10 +36,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener("DOMContentLoaded", function() {
     const submitButton = document.getElementById('submit-button-scales');
-    submitButton.addEventListener('click', function(event) {
-        event.preventDefault();  // Prevents form submission and page reload
-        checkAnswer();
-    });
+    //submitButton.addEventListener('click', function(event) {
+    //    event.preventDefault();  // Prevents form submission and page reload
+    //    checkAnswer();
+    //});
 });
 
 function checkAnswer() {
@@ -64,10 +65,10 @@ function checkAnswer() {
 
 document.addEventListener("DOMContentLoaded", function() {
     const submitButton = document.getElementById('submit-button-major');
-    submitButton.addEventListener('click', function(event) {
-        event.preventDefault();  // Prevents form submission and page reload
-        checkAnswer2();
-    });
+    //submitButton.addEventListener('click', function(event) {
+    //    event.preventDefault();  // Prevents form submission and page reload
+    //    checkAnswer2();
+    //});
 });
 
 function checkAnswer2() {
@@ -93,10 +94,10 @@ function checkAnswer2() {
 
 document.addEventListener("DOMContentLoaded", function() {
     const submitButton = document.getElementById('submit-button-id2');
-    submitButton.addEventListener('click', function(event) {
-        event.preventDefault();  // Prevents form submission and page reload
-        checkAnswer3();
-    });
+    //submitButton.addEventListener('click', function(event) {
+    //    event.preventDefault();  // Prevents form submission and page reload
+    //    checkAnswer3();
+    //});
 });
 
 function checkAnswer3() {
@@ -126,10 +127,10 @@ function checkAnswer3() {
 
 document.addEventListener("DOMContentLoaded", function() {
     const submitButton = document.getElementById('submit-4');
-    submitButton.addEventListener('click', function(event) {
-        event.preventDefault();  // Prevents form submission and page reload
-        checkAnswer4();
-    });
+    //submitButton.addEventListener('click', function(event) {
+    //    event.preventDefault();  // Prevents form submission and page reload
+    //    checkAnswer4();
+    //});
 });
 
 function checkAnswer4() {
@@ -155,10 +156,99 @@ function checkAnswer4() {
     }
 }
 
+let notes = [
+    "a0",
+    "a-0",
+    "a1",
+    "a-1",
+    "a2",
+    "a-2",
+    "a3",
+    "a-3",
+    "a4",
+    "a-4",
+    "a5",
+    "a-5",
+    "a6",
+    "a-6",
+    "a7",
+    "a-7",
+    "b0",
+    "b1",
+    "b2",
+    "b3",
+    "b4",
+    "b5",
+    "b6",
+    "b7",
+    "c1",
+    "c-1",
+    "c2",
+    "c-2",
+    "c3",
+    "c-3",
+    "c4",
+    "c-4",
+    "c5",
+    "c-5",
+    "c6",
+    "c-6",
+    "c7",
+    "c-7",
+    "c8",
+    "d1",
+    "d-1",
+    "d2",
+    "d-2",
+    "d3",
+    "d-3",
+    "d4",
+    "d-4",
+    "d5",
+    "d-5",
+    "d6",
+    "d-6",
+    "d7",
+    "d-7",
+    "e1",
+    "e2",
+    "e3",
+    "e4",
+    "e5",
+    "e6",
+    "e7",
+    "f1",
+    "f-1",
+    "f2",
+    "f-2",
+    "f3",
+    "f-3",
+    "f4",
+    "f-4",
+    "f5",
+    "f-5",
+    "f6",
+    "f-6",
+    "f7",
+    "f-7",
+    "g1",
+    "g-1",
+    "g2",
+    "g-2",
+    "g3",
+    "g-3",
+    "g4",
+    "g-4",
+    "g5",
+    "g-5",
+    "g6",
+    "g-6",
+    "g7",
+    "g-7"
+]
 
-
-
-
-
-
-
+$(document).ready(function() {
+    notes.forEach((note) => $('#'+note).click( function() {
+        playNote(note);
+    }));
+})
