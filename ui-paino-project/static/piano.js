612628d15e454ade04aba63d8d6058f23d590640
Michael Yakubov <michael.yakubov22@gmail.com>
2024-05-03 21:17:24 -0400

    add demo

--- ui-paino-project/static/piano.js
+++ ui-paino-project/static/piano.js
@@ -5,12 +5,41 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function playNote(note) {
-    console.log("Playing note:", note);
     const audio = new Audio(`/static/media/sounds/${note}.mp3`); // Ensure you have .wav files named as notes (C.wav, C#.wav, etc.)
     audio.play();
 }
 
+function playDemo(noteList){
+    const buttonElement = document.querySelector('#demoButton');
+    buttonElement.disabled = true;
+    let notes = noteList.split(',');
 
+    function playSequentially(index) {
+        if (index < notes.length) {
+            const note = notes[index].trim();
+            const keyElement = document.querySelector(`[data-note="${note}"]`);
+            console.log(keyElement);
+            keyElement.classList.add('active');
+            playNoteWithDelay(note, 500).then(() => {
+                keyElement.classList.remove('active');
+                playSequentially(index + 1);
+                if (index === notes.length - 1) {
+                    buttonElement.disabled = false;
+                }
+            });
+        }
+    }
+    playSequentially(0);
+}
+
+function playNoteWithDelay(note, delay) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            playNote(note);
+            resolve();
+        }, delay);
+    });
+}
 
 
 
